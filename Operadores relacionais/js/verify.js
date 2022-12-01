c159cc4419c923b6f6b4101ece793fa6660b1b09
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-12-01 19:03:23 -0300

    Operadores relacionais

---
+++ Operadores relacionais/js/verify.js
@@ -0,0 +1,35 @@
+let yearOfBirth = document.getElementById("yearOfBirth")
+let feedback = document.getElementById("feedback")
+let withMajor = document.getElementById("withMajor")
+let year = new Date().getFullYear()
+
+document.getElementById("btnVerify").addEventListener("click", () => {
+    let inputValue = yearOfBirth.value
+    let maxYear = year - 100
+
+    if (inputValue == "") {
+        // Se for verdade faz isso aqui
+        yearOfBirth.classList.add("is-invalid")
+        feedback.innerText = "O campo não pode ficar em branco!!!"
+    } else if (parseInt(inputValue) < maxYear) {
+        yearOfBirth.classList.add("is-invalid")
+        feedback.innerText = `A data informada é inferior ao ano ${maxYear}!!!`
+    }else {
+        yearOfBirth.classList.remove("is-invalid")
+        yearOfBirth.classList.add("is-valid")
+
+        let age = year - parseInt(inputValue)
+        verifyAccess(age)
+    }
+})
+
+function verifyAccess(age) {
+    if(age >= 18){
+        alert("Seja bem-vindo ao nosso sistema!!!")
+    } else if (age >=16 && withMajor.checked) {
+        alert("Sejam bem-vindos ao nosso sistema!!!")
+    }else {
+        alert("Lamento, você não está autorizado a entrar!!!")
+    }
+    yearOfBirth.classList.remove("is-valid")
+}
